704fa52d0dbdf1f3e1d60a48af03c3d8ff1a87f9
裘洪睿 <jackqiuhongrui@outlook.com>
2023-12-28 10:02:34 +0800

    py on web

---
+++ runit.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Dense
+from keras.datasets import mnist
+
+import numpy as np
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
